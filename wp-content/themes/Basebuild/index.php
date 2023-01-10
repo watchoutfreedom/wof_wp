25f08e9e565a8b64b390f4804b000d64cf688f7b
biip <bm.palma@clix.pt>
2023-01-10 15:42:37 +0000

    home

--- wp-content/themes/Basebuild/index.php
+++ wp-content/themes/Basebuild/index.php
@@ -1,31 +1,48 @@
 <?php
 /**
- * The main template file
+ * The template for displaying the blog
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * e.g., it puts together the home page when no home.php file exists.
  *
- * Learn more: {@link https://codex.wordpress.org/Template_Hierarchy}
+ * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package Ollie Smith Basebuild
- * @since wpbb 1.0.0
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
  */
 
 get_header(); ?>
 
-<div class="page">
-<?php   get_template_part( 'template-parts/content', get_post_type() ); ?>
-    <section class="section">
-        <div class="content">
-            <div class="row">
-                <div class="col-sm-12">
-                    <?php the_content(); ?>
-                </div>
-            </div>
-        </div>
-    </section>
-    <?php  get_template_part( 'template-parts/content', get_post_type() ); ?>
+<h4>Un blog abierto a las ideas de nuetros colaboradores.</h4>
+
+<div class="page main-container">
+	<?php if ( have_posts() ) : ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<section>
+				<div class="">
+					<a href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a>
+                    <div class="author"><span><?php the_author_meta( 'user_nicename' , the_author() ); ?></span></div>
+					<a href="<?php the_permalink() ?>"><h1><?php the_title(); ?></h1></a>
+					<p><?php echo get_field('excerpt',get_the_ID()) ?></p>
+					<hr>
+				</div>
+    		</section>
+
+		<?php endwhile; ?>
+
+	<?php else : ?>
+		<?php get_template_part( 'template-parts/none' ); ?>
+
+	<?php endif; // End have_posts() check. ?>
+	<?php
+	/* Display navigation to next/previous pages when applicable */
+	if ( function_exists( 'foundationpress_pagination' ) ) :
+		foundationpress_pagination();
+	elseif ( is_paged() ) :
+	?>
+		<nav id="post-nav">
+			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
+			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
+		</nav>
+	<?php endif; ?>
 </div>
+
 <?php get_footer();
