b61bf3e9ebd8834c2f20070608b282425f46e45f
biip <bm.palma@clix.pt>
2023-01-13 11:49:56 +0000

    page donate

---
+++ wp-content/themes/Basebuild/page-templates/page-donate.php
@@ -0,0 +1,27 @@
+<?php
+/**
+* Template Name: Donate
+*
+*/
+
+get_header(); ?>
+
+<div class="page main-container">
+    <section>
+    <p>
+    <?php the_content() ?>
+    </p>
+    </section>
+    <div>
+        <a href="#" class="button">STRIPE</a>
+    </div>
+    <div>
+        <a href="#" class="button">BTC</a>
+    </div>
+    <p>O puedes  aportar con un  ingreso directo en la cuenta: ES193139000313
+    </p>
+    <a class="button grey" href="/colabora">VOLVER</a>
+</div>
+
+<?php wp_footer(); ?>
+
