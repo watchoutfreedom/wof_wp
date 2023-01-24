8002ba7bd86b7ce6a5a6bcdab946b0a8efcee115
biip <bm.palma@clix.pt>
2023-01-24 15:29:52 +0000

    fix

--- wp-content/themes/Basebuild/page-templates/page-colabora.php
+++ wp-content/themes/Basebuild/page-templates/page-colabora.php
@@ -8,16 +8,17 @@ get_header(); ?>
 
 <div class="page main-container">
     <section>
-    <p>
-        ¡ESTUPENDO! <br>
-        Tienes dos maneras de cooperar.<br>
-        Puedes suscribirte como colaborador y  te diremos cómo aportar tu conocimiento y/o talento
-    </p>
-    <a href="/signup">COLABORAR</a>
+        <p>
+            ¡ESTUPENDO! <br>
+            Tienes dos maneras de cooperar.<br>
+            Puedes suscribirte como colaborador y  te diremos cómo aportar tu conocimiento y/o talento
+        </p>
+        <a href="/signup">COLABORAR</a>
     </section>
-
-    <p>O puedes  aportar con una cuota anual o donación</p>
-    <a href="/donate">DONAR</a>
+    <section>
+        <p>O puedes  aportar con una cuota anual o donación</p>
+        <a href="/donate">DONAR</a>
+    </section>
 </div>
 
 <?php wp_footer(); ?>
