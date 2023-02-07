35f773fd88f9b78bb6aa8d142064790d94091af1
biip <bm.palma@clix.pt>
2023-02-07 17:13:25 +0000

    lazy load

--- wp-content/themes/Basebuild/library/repeater-template.php
+++ wp-content/themes/Basebuild/library/repeater-template.php
@@ -1,6 +1,24 @@
-<a href="<?php echo get_permalink() ?>"><?php echo get_the_post_thumbnail(); ?></a>
-<div class="post_type"><label for=""><?php echo get_post_type() ?></label></div>
-<h2><a href="<?php echo get_permalink() ?>"><?php echo the_title() ?></a></h2>
-<p><?php echo get_field('excerpt') ?></p>
-<div class="author"><span><?php the_author(); ?></span></div>
-<hr>
+<section>
+    <a href="<?php echo get_permalink() ?>"><?php echo the_post_thumbnail(); ?></a>
+    <div class="post_type"><label for=""><?php echo get_post_type() ?></label></div>
+    <div class="subtitle">
+        <label for="">
+            <?php 
+            if($post_type == 'activity'){
+                if(get_field('location',get_the_ID())) echo get_field('location',get_the_ID()); 
+                if(get_field('initial_date',get_the_ID())) echo " | ".get_field('initial_date',get_the_ID());
+                if(get_field('end_date',get_the_ID())) echo " - ".get_field('end_date',get_the_ID());
+
+            }
+            if($post_type == 'service' || $post_type = 'product')
+                if(get_field('type',get_the_ID())) echo get_field('type',get_the_ID()); 		
+            ?>
+		</label>
+    </div>
+    <h2><a href="<?php echo get_permalink() ?>"><?php echo the_title() ?></a></h2>
+    <p><?php echo get_field('excerpt') ?></p>
+    <div class="author">
+        <span><?php the_author_meta( 'user_nicename' , the_author() ); ?></span>
+    </div>
+    <hr>
+</section>
