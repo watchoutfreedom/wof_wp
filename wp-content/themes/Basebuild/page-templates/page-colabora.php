a170dcac84ad656de4109d4fb8d7b53c007ef943
biip <bm.palma@clix.pt>
2023-02-07 12:38:53 +0000

    update colabora page

--- wp-content/themes/Basebuild/page-templates/page-colabora.php
+++ wp-content/themes/Basebuild/page-templates/page-colabora.php
@@ -9,15 +9,24 @@ get_header(); ?>
 <div class="page main-container">
     <section>
         <p>
-            ¡ESTUPENDO! <br>
-            Tienes dos maneras de cooperar.<br>
-            Puedes suscribirte como colaborador y  te diremos cómo aportar tu conocimiento y/o talento
+            ¡ESTUPENDO! <br><br>
+            Tienes varias maneras de cooperar.<br><br>
+            Puedes asociarte con una cuota anual o donación.
         </p>
-        <a href="/signup">COLABORAR</a>
+        <a class="button" href="/donate">DONAR</a>
     </section>
     <section>
-        <p>O puedes  aportar con una cuota anual o donación</p>
-        <a href="/donate">DONAR</a>
+        <p>
+            Puedes comprar algún artículo en nuestra tienda.
+        </p>
+        <a href="/shop" class="button">VER TIENDA</a>
+    </section>
+    <section>
+        <p>Puedes suscribirte como colaborador y  te diremos cómo aportar tu conocimiento y/o talento</p>
+        <a class="button" href="/signup">UNIRME</a>
+    </section>
+    <section>
+        <a href="/comunidad" class="button grey">CONSULTAR LISTA DE COLABORADORES</a>
     </section>
 </div>
 
