f470d401f716a7bf8f360210eed7f1d1fb7fdc52
biip <bm.palma@clix.pt>
2023-02-07 17:12:52 +0000

    lazy load

--- wp-content/themes/Basebuild/archive.php
+++ wp-content/themes/Basebuild/archive.php
@@ -22,49 +22,10 @@ get_header(); ?>
 <h4><?php echo get_the_post_type_description(); ?></h4>
 
 <div class="page main-container">
-	<?php if ( have_posts() ) : ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<section>
-				<div class="">
-					<a href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a>
-					<div class="subtitle">
-						<label for="">
-							<?php 
-							if($post_type == 'activity'){
-								if(get_field('location',get_the_ID())) echo get_field('location',get_the_ID()); 
-								if(get_field('initial_date',get_the_ID())) echo " | ".get_field('initial_date',get_the_ID());
-								if(get_field('end_date',get_the_ID())) echo " - ".get_field('end_date',get_the_ID());
-
-							}
-							if($post_type == 'service' || $post_type = 'product')
-								if(get_field('type',get_the_ID())) echo get_field('type',get_the_ID()); 		
-							?>
-						</label>
-					</div>
-					<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-					<p><?php echo get_field('excerpt',get_the_ID()) ?></p>
-					<div class="author"><span><?php the_author_meta( 'user_nicename' , the_author() ); ?></span></div>
-					<hr>
-				</div>
-    		</section>
-
-		<?php endwhile; ?>
-
-	<?php else : ?>
-		<?php get_template_part( 'template-parts/none' ); ?>
-
-	<?php endif; // End have_posts() check. ?>
-	<?php
-	/* Display navigation to next/previous pages when applicable */
-	if ( function_exists( 'foundationpress_pagination' ) ) :
-		foundationpress_pagination();
-	elseif ( is_paged() ) :
-	?>
-		<nav id="post-nav">
-			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-		</nav>
-	<?php endif; ?>
+	<?php 
+    //lazy load
+    echo do_shortcode('[ajax_load_more id="6962705037" loading_style="infinite classic" post_type="'.$post_type.'" posts_per_page="5" post_format="standard"]') 
+    ?>
 </div>
 
 <?php get_footer();
