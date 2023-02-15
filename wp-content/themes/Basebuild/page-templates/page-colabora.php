3e9264b5510da91a1aafc76290c3b7eb2480857f
watchoutfreedom <watchoutfreedom@gmail.com>
2023-02-15 12:12:26 +0100

    colabora info

--- wp-content/themes/Basebuild/page-templates/page-colabora.php
+++ wp-content/themes/Basebuild/page-templates/page-colabora.php
@@ -10,10 +10,25 @@ get_header(); ?>
     <section>
         <p>
             ¡ESTUPENDO! <br><br>
-            Tienes varias maneras de cooperar.<br><br>
-            Puedes asociarte con una cuota anual o donación.
+            Wof! es una asociación sin animo de lucro dedicada a fomentar la cooperación social, el debate y la formación en materias afectadas por la desinformación o el desinterés del statu quo. Creamos proyectos abiertos a todos aquellos que deseen participar. 
+            Nuestro principio: Crear plataformas que permitan confrontar pacíficamente lo diferente, incluso lo opuesto, nos ayuda a mejorar como sociedad.
+            Si te interesa ser parte o ayudar en algún proyecto puedes hacerlo de las siguientes maneras:
+
+            La más sencilla: Síguiéndonos en redes sociales y compartiendo nuestras publicaciones. Twitter(LINK), Youtube(Link), Instagram(Link)
+        </p>
+    </section>
+    <section>
+        <p>
+            Puedes asociarte con una cuota anual o donación. Donando 1 eur al mes (LINK), tendrás acceso a la información sobre los eventos que organicemos, además de otras ventajas.
         </p>
         <a class="button" href="/donate">DONAR</a>
+    </section>
+    <section>
+         <p>
+        Como interlocutor o suscriptor. Nuestro blog permite responder a un artículo con otro artículo en lugar de escribir comentarios. El objetivo es mejorar la calidad del debate; no hace falta que sea una respuesta extensa pero sí reflexiva. Cualquier lector puede proponerla (moderada por los colaboradores) convirtiéndose en interlocutor.
+        Como suscriptor recibirás las noticias sobre nuestra actividad y podrás enterarte antes que nadie de las iniciativas.
+        <a href="/shop" class="button">SUSCRIBIRME</a>
+        </p>
     </section>
     <section>
         <p>
@@ -22,7 +37,10 @@ get_header(); ?>
         <a href="/shop" class="button">VER TIENDA</a>
     </section>
     <section>
-        <p>Puedes suscribirte como colaborador y  te diremos cómo aportar tu conocimiento y/o talento</p>
+        <p>Puedes suscribirte como colaborador y te ayudaremos a aportar tu conocimiento y/o talento.
+        Queremos unir a personas dedicadas a diferentes disciplinas para que colaboren, además de crear brainstorms comunes en los que cualquiera puede formar parte. Si creamos plataformas que permitan unir lo diferente, incluso lo opuesto, podremos avanzar como sociedad.
+        Si tienes un tema que te interese y crees que puedes aportar conocimiento a los demás, únete como colaborador y te daremos plataforma y apoyo para que transmitas lo que sabes. Tendrás acceso a la participación directa en la plataforma y proyectos que elijas, publicación en el blog, la toma de decisiones y por supuesto acceso preferente en nuestras actividades.   
+        </p>
         <a class="button" href="/signup">UNIRME</a>
     </section>
     <section>
