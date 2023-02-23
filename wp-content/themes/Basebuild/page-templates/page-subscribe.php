f0240f26e1cc4bcc72db73fe8b5de8622a040646
biip <bm.palma@clix.pt>
2023-02-23 00:06:56 +0000

    add filters

---
+++ wp-content/themes/Basebuild/page-templates/page-subscribe.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Template Name: Subscribe
+*/
+
+
+
+acf_form_head();
+
+get_header();?>
+
+<div class="page main-container">
+
+<?php if(!$_POST['email']){ ?>
+<section>
+    <p>Escribe tu email y recibirás las últimas noticias sobre nuestra actividad y proyectos.</p>
+
+    <form method="post">
+        <input type="email" name="email" placeholder="email">
+        <input type="email" name="email2" placeholder="repetir email">
+        <input type="submit" value="submit" class="button">
+    </form>
+</section>
+<?php } 
+else{?>
+
+<section><p>¡GRACIAS! 
+
+Además, puedes asociarte a Wof! y  ayúdanos a difundir información de calidad en temas clave de nuetro tiempo, además de impulsar iniciativas de cooperación social entre diferentes ideologías.  Entérate antes de nuestras oprortunidades  y ventajas  para ti.</p></section>    
+
+<?php }
+
+
+?>
+</div>
+
+<?php wp_footer(); ?>
