75004c6e555049176e135a11d33b42bdf85c0a9a
watchoutfreedom <watchoutfreedom@gmail.com>
2023-03-15 10:28:48 +0100

    colabora-descript

--- wp-content/themes/Basebuild/page-templates/page-colabora.php
+++ wp-content/themes/Basebuild/page-templates/page-colabora.php
@@ -10,8 +10,7 @@ get_header(); ?>
     <section>
         <p>
             ¡Gracias por interesarte en colaborar! 
-            Wof! es una asociación sin animo de lucro dedicada a fomentar la cooperación social, el debate y la formación en materias afectadas por la desinformación o el desinterés del statu quo. 
-            Creamos plataformas que permitan confrontar pacíficamente las diferencias.
+            Wof! es una asociación sin animo de lucro dedicada a fomentar la cooperación social, el debate y la formación en materias afectadas por la desinformación o el desinterés del statu quo.            
             Si te interesa ser parte o ayudar en algún proyecto puedes hacerlo de las siguientes maneras:
 
             La más sencilla: Síguiéndonos en redes sociales y compartiendo nuestras publicaciones. Twitter(LINK), Youtube(Link), Instagram(Link)
@@ -19,16 +18,16 @@ get_header(); ?>
     </section>
     <section>
         <p>
-            Puedes asociarte con una cuota anual o donación. Donando 1 eur al mes (LINK), tendrás acceso a la información sobre los eventos que organicemos, además de otras ventajas.
+            También puedes asociarte con una cuota anual o donación. Donando 1 eur al mes (LINK), nos ayudarás a organizar actividades que aporten conocimiento de calidad a otras personas, además de obtener ciertas ventajas.
         </p>
         <a class="button" href="/donate">DONAR</a>
     </section>
     <section>
          <p>
-        Como interlocutor o suscriptor. Nuestro blog permite responder a un artículo con otro artículo en lugar de escribir comentarios. El objetivo es mejorar la calidad del debate; no hace falta que sea una respuesta extensa pero sí reflexiva. Cualquier lector puede proponerla (moderada por los colaboradores) convirtiéndose en interlocutor.
-        Como suscriptor recibirás las noticias sobre nuestra actividad y podrás enterarte antes que nadie de las iniciativas.
-        <a href="/shop" class="button">SUSCRIBIRME</a>
+        Como interlocutor o suscriptor. Cualquier lector puede proponer una respuesta a una publicación (moderada por los colaboradores) convirtiéndose en interlocutor de un debate libre que queremos fomentar en nuestra web.
+        Como suscriptor recibirás las noticias sobre nuestra actividad y podrás enterarte antes de las iniciativas.
         </p>
+        <a href="/shop" class="button">SUSCRIBIRME</a>
     </section>
     <section>
         <p>
@@ -37,14 +36,13 @@ get_header(); ?>
         <a href="/shop" class="button">VER TIENDA</a>
     </section>
     <section>
-        <p>Puedes suscribirte como colaborador y te ayudaremos a aportar tu conocimiento y/o talento.
-        Queremos unir a personas dedicadas a diferentes disciplinas para que colaboren, además de crear brainstorms comunes en los que cualquiera puede formar parte. Si creamos plataformas que permitan unir lo diferente, incluso lo opuesto, podremos avanzar como sociedad.
-        Si tienes un tema que te interese y crees que puedes aportar conocimiento a los demás, únete como colaborador y te daremos plataforma y apoyo para que transmitas lo que sabes. Tendrás acceso a la participación directa en la plataforma y proyectos que elijas, publicación en el blog, la toma de decisiones y por supuesto acceso preferente en nuestras actividades.   
+        <p>
+        Queremos unir a personas dedicadas a diferentes disciplinas para debatir y cooperar. 
+        Si tienes un tema que te interese y crees que puedes aportar conocimiento a los demás, contáctanos. Tendrás acceso a la participación directa en la plataforma y proyectos que elijas, publicación en el blog, toma de decisiones y acceso preferente a las actividades.   
         </p>
         <a class="button" href="/signup">UNIRME</a>
-    </section>
-    <section>
         <a href="/comunidad" class="button grey">CONSULTAR LISTA DE COLABORADORES</a>
+
     </section>
 </div>
 
