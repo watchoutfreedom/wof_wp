669eb13ed528561326231df8f4b5012fd6d6ed71
biip <bm.palma@clix.pt>
2023-04-05 21:21:43 +0100

    redirect to previous answer page

--- wp-content/themes/Basebuild/page-templates/page-signup.php
+++ wp-content/themes/Basebuild/page-templates/page-signup.php
@@ -13,9 +13,20 @@ get_header();?>
 
 <div class="page main-container">
 
-    <?php acf_form([
+    <?php 
+    
+    if (get_transient('originalRegisterRefererURL') ){
+        $redirect = get_transient('originalRegisterRefererURL');
+        delete_transient('originalRegisterRefererURL');
+    }
+    else
+        $redirect = home_url();
+    
+    
+    acf_form([
             'field_groups' => [ 283 ],
             'post_id'      => 'new_user',
+            'return' => $redirect,
             'updated_message' => __("Solicitud registrada. Confirma la suscripción en tu email", 'acf'),
             'submit_value' => __("UNIRME", 'acf')]);
     ?>
