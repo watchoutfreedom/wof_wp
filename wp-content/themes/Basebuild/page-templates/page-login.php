85df680f917f54ac93ec5567b134cc5fe926570a
biip <bm.palma@clix.pt>
2023-04-12 16:06:08 +0100

    fix lost password

--- wp-content/themes/Basebuild/page-templates/page-login.php
+++ wp-content/themes/Basebuild/page-templates/page-login.php
@@ -20,37 +20,57 @@
 
 if ( ! is_user_logged_in() ) {
 
-    if( isset($_GET['action']) && $_GET['action'] == 'lostpassword'){
-        
-    }
-
-    if (get_transient('originalRegisterRefererURL') ){
-    $redirect = home_url()."/create-post/?action=create&id=".url_to_postid(get_transient('originalRegisterRefererURL'));
-    }
-    else
-    $redirect = home_url();
-
-
-    $args = array(
-        'redirect' => $redirect, // redirect to admin dashboard.
-        'form_id' => 'custom_loginform',
-        'label_username' => false,
-        'label_password' => false,
-        'label_remember' => __( 'Remember Me' ),
-        'label_log_in' => __( 'Log In' ),
-         'remember' => false,
-         'echo' => false
-    );
-
-    $form = wp_login_form( $args );
-
-    //add the placeholders
-    $form = str_replace('name="log"', 'name="log" placeholder="Username"', $form);
-    $form = str_replace('name="pwd"', 'name="pwd" placeholder="Password"', $form);
+    if( isset($_GET['action']) && $_GET['action'] == 'lostpassword'){?>
     
-    echo $form;
-    echo "<a href='/login?action=lostpassword'>Recuperar contraseña</a>";
-    echo "<div><a href='/signup'>No tienes cuenta? Únete!</a></div>";
+
+        <div id="password-lost-form">
+            <h3><?php _e( 'Forgot Your Password?', 'personalize-login' ); ?></h3>
+        <p>
+            <?phpe("Enter your email address and we'll send you a link you can use to pick a new password.",'personalize_login');?>
+        </p>
+
+        <form id="lostpasswordform" action="<?php echo wp_lostpassword_url(); ?>" method="post">
+                <input type="text" name="user_login" id="user_login" placeholder="Email">
+                <br>
+                <input type="submit" name="submit" class="button" value="<?php _e( 'Reset Password', 'personalize-login' ); ?>"/>
+        </form>
+        
+        <?php if($error) echo display_error_message($error);?>
+
+        </div>
+
+    <?php }
+
+    else{
+
+        if (get_transient('originalRegisterRefererURL') ){
+        $redirect = home_url()."/create-post/?action=create&id=".url_to_postid(get_transient('originalRegisterRefererURL'));
+        }
+        else
+        $redirect = home_url();
+
+
+        $args = array(
+            'redirect' => $redirect, // redirect to admin dashboard.
+            'form_id' => 'custom_loginform',
+            'label_username' => false,
+            'label_password' => false,
+            'label_remember' => __( 'Remember Me' ),
+            'label_log_in' => __( 'Log In' ),
+            'remember' => false,
+            'echo' => false
+        );
+
+        $form = wp_login_form( $args );
+
+        //add the placeholders
+        $form = str_replace('name="log"', 'name="log" placeholder="Username"', $form);
+        $form = str_replace('name="pwd"', 'name="pwd" placeholder="Password"', $form);
+        
+        echo $form;
+        echo "<a href='/login?action=lostpassword'>Recuperar contraseña</a>";
+        echo "<div><a href='/signup'>No tienes cuenta? Únete!</a></div>";
+    }
 
     
 }
