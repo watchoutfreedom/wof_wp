9a75eb35bbcad04a350ff526e976c9ab658289bf
watchoutfreedom <watchoutfreedom@gmail.com>
2023-04-28 17:19:06 +0200

    css updates

--- wp-content/themes/Basebuild/header.php
+++ wp-content/themes/Basebuild/header.php
@@ -18,6 +18,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" defer></script>
   <link href='https://fonts.googleapis.com/css?family=Coco+Goose' rel='stylesheet'>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,200;0,500;0,800;1,200;1,500;1,800&display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" />
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" defer></script>
