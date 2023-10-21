c2a9464de5ace317f3357224c953c88f9fbd837c
biip <bm.palma@clix.pt>
2023-10-21 19:00:04 +0100

    add stripe scripts

--- wp-content/themes/Basebuild/page-templates/page-colabora.php
+++ wp-content/themes/Basebuild/page-templates/page-colabora.php
@@ -16,11 +16,18 @@ get_header(); ?>
 
     </section>
     <section>
-        <h2>1. Puedes asociarte con una cuota anual o donación.</h2>
+        <h2>1. Puedes asociarte con una cuota o donación.</h2>
         <p>
-            Puedes asociarte con una cuota anual o donación. Tendrás acceso a la información sobre los eventos que organicemos, además de otras ventajas.
+            Puedes asociarte con una cuota o donación. Tendrás acceso a la información sobre las iniciativas, además de otras ventajas.
         </p>
-        <a class="button" href="https://buy.stripe.com/cN23cd1uQ4k7cBGeV2">ASOCIARME</a>
+        <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
+        <stripe-pricing-table pricing-table-id="prctbl_1O3HmgGsxSUQmlTTVSuEq2Rw"
+        publishable-key="pk_live_51Kz4S0GsxSUQmlTTi5auNcSejQ4sdpPC14eSiC9JuFJgyDHTlC2zjYPzBcq6G87JL7ykySo0imi3WeYI6AbaC42i00JcRtgfvK">
+        </stripe-pricing-table>
+
+        <p>
+            O puedes donar libremente en FIAT o BTC:
+        </p>
         <a class="button" href="/donate">DONAR</a>
     </section>
     <section>
